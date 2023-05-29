af6b21fcdf9bf93dc6f6faaa16f09cfa12676fc4
dkaula <133680228+dkaula@users.noreply.github.com>
2023-05-29 07:49:50 +0200

    Add functions.php for Class auto registering

---
+++ functions.php
@@ -0,0 +1,16 @@
+<?php
+
+require_once ('base_config.php'); // load base config with paths to classes etc.
+
+function load_model($class_name)
+{
+	global $baseClassModelDir;
+	$path_to_file = $baseClassModelDir. $class_name . '.php';
+	$allowed_include = 1; // set allow to 1 in order to include the user.php script
+	if (file_exists($path_to_file)) {
+		require $path_to_file;
+	}
+}
+
+spl_autoload_register('load_model');
+?>
