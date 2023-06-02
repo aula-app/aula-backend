c04259484835ef15890a3e04761d2d394cd72e7b
dkaula <133680228+dkaula@users.noreply.github.com>
2023-06-02 14:15:26 +0200

    Example Code for using model idea

---
+++ query_idea.php
@@ -0,0 +1,270 @@
+<?php
+require_once ('base_config.php'); // load base config with paths to classes etc.
+require_once ('error_msg.php');
+
+require ('functions.php'); // include Class autoloader (models)
+
+//load helper classes
+require_once ($baseHelperDir.'Crypt.php');
+
+
+
+
+/* */
+// Create a new Database object with the MySQL credentials
+$db = new Database();
+$crypt = new Crypt($cryptFile); // path to $cryptFile is currently known from base_config.php -> will be changed later to be secure
+$syslog = new Systemlog ($db); // systemlog
+$idea = new Idea ($db, $crypt, $syslog); // instanciate group model class
+$room = new Room ($db, $crypt, $syslog); // instanciate room model class
+
+
+
+function out ($text, $form=false){ // lazy helper function
+  $formstart="";
+  $formend="";
+
+  if ($form) {
+    $formstart="<h2>";
+    $formend="</h2>";
+  }
+
+  echo ("<br>".$formstart.$text.$formend."<br>");
+}
+
+
+out ("EXAMPLES FOR USING THE MODELS (in this case...ROOMS and GROUPS)",true);
+/* */
+// Example for adding a room in the database
+/* */
+
+
+
+$user = new User($db, $crypt, $syslog);
+
+// create a random appendix to have different users....
+$testrand = rand (100,1000);
+$appendix = microtime(true).$testrand;
+
+
+$ideas = array();
+
+// Random ideas to populate the array
+$randomIdeas = array(
+    "Create a mobile app for productivity",
+    "Design a website for online learning",
+    "Develop a game with augmented reality",
+    "Build a social media platform for pets",
+    "Start a podcast about technology trends",
+    "Write a book on personal development",
+    "Invent a smart device for home automation",
+    "Launch an e-commerce store for handmade crafts",
+    "Organize a charity event for a good cause",
+    "Create a video tutorial series on coding",
+    "Design a new logo for a local business",
+    "Start a YouTube channel about travel experiences",
+    "Develop a fitness app with personalized workout plans",
+    "Build a community forum for music enthusiasts",
+    "Create an online marketplace for renting tools",
+    "Write a screenplay for a short film",
+    "Invent a new board game for family entertainment",
+    "Organize a virtual conference on sustainability",
+    "Develop a language learning app with interactive lessons",
+    "Start a blog about healthy cooking recipes",
+    "Design a fashion line using sustainable materials",
+    "Build a platform for connecting freelance writers with clients",
+    "Create a podcast network featuring various genres",
+    "Develop a virtual reality game for educational purposes",
+    "Start a mentoring program for aspiring entrepreneurs",
+    "Build an app for tracking personal finances",
+    "Design a website for connecting pet owners with pet sitters",
+    "Create an online course on digital marketing",
+    "Develop a mobile app for finding local hiking trails",
+    "Start a photography business specializing in portrait shots",
+    "Build a platform for connecting language exchange partners",
+    "Create a social networking app for book lovers",
+    "Design a travel itinerary planner with personalized recommendations",
+    "Start a web development agency for small businesses",
+    "Develop a mindfulness meditation app",
+    "Build a platform for online tutoring sessions",
+    "Create a virtual reality experience for exploring historical landmarks",
+    "Design a subscription box service for self-care products",
+    "Start a podcast about personal finance and investment strategies",
+    "Build a mobile app for tracking daily habits and goals",
+    "Develop a platform for sharing and discovering local artwork",
+    "Create a website for connecting volunteers with nonprofit organizations",
+    "Design a mobile game based on solving puzzles",
+    "Start an online store selling eco-friendly clothing",
+    "Build a platform for connecting musicians for collaboration",
+    "Develop a meditation app for children",
+    "Create an online platform for personalized fitness coaching",
+    "Design a mobile app for finding the best deals and discounts",
+    "Start a YouTube channel about DIY home improvement projects",
+    "Build a platform for sharing and discovering new music",
+    "Develop a virtual reality training program for medical students",
+    "Create a social networking app for connecting outdoor enthusiasts",
+    "Design a website for sharing travel experiences and tips",
+    "Start a podcast about mental health and self-care",
+    "Build a platform for connecting remote freelance designers with clients",
+    "Develop a mobile app for learning musical instruments",
+    "Create an online store for handmade jewelry",
+    "Design a mobile game that promotes environmental awareness",
+    "Start a blog about sustainable living and zero-waste lifestyle",
+    "Build a platform for sharing and discovering healthy recipes",
+    "Develop a language translation app with real-time voice recognition",
+    "Create a social networking app for connecting professionals in a specific industry",
+    "Design a website for booking and reviewing local services",
+    "Start a podcast about the history of art",
+    "Build a platform for connecting travelers with local tour guides",
+    "Develop a mobile app for practicing mindfulness and meditation",
+    "Create an online marketplace for second-hand goods",
+    "Design a mobile game that helps improve memory and cognitive skills",
+    "Start a blog about personal development and motivation",
+    "Build a platform for sharing and discovering short stories",
+    "Develop a mobile app for managing personal budget and expenses",
+    "Create a social networking app for connecting food enthusiasts",
+    "Design a website for finding and booking vacation rentals",
+    "Start a podcast about current trends in technology and innovation",
+    "Build a platform for connecting freelance photographers with clients",
+    "Develop a language learning app focused on slang and colloquial expressions",
+    "Create an online store for eco-friendly household products",
+    "Design a mobile game that teaches coding concepts",
+    "Start a blog about sustainable fashion and ethical clothing brands",
+    "Build a platform for sharing and discovering travel itineraries",
+    "Develop a mobile app for mental health tracking and support",
+    "Create a social networking app for connecting artists and art enthusiasts",
+    "Design a website for sharing and reviewing local restaurants",
+    "Start a podcast about personal stories of overcoming adversity",
+    "Build a platform for connecting pet owners with veterinarians",
+    "Develop a mobile app for learning and practicing yoga",
+    "Create an online marketplace for digital artwork",
+    "Design a mobile game that promotes physical fitness and exercise",
+    "Start a blog about personal finance and money-saving tips",
+    "Build a platform for sharing and discovering inspirational quotes",
+    "Develop a mobile app for finding and joining local sports activities",
+    "Create a social networking app for connecting music producers and artists",
+    "Design a website for sharing and reviewing movies and TV shows",
+    "Start a podcast about science and technology advancements",
+    "Build a platform for connecting remote freelance writers with clients",
+    "Develop a language learning app focused on conversational skills",
+    "Create an online store for organic and natural beauty products",
+    "Design a mobile game that promotes environmental conservation",
+    "Start a blog about healthy eating and plant-based diets",
+    "Build a platform for sharing and discovering workout routines",
+    "Develop a mobile app for finding and attending local art exhibitions",
+    "Create a social networking app for connecting gamers",
+    "Design a website for sharing and discussing current news topics",
+    "Start a podcast about entrepreneurship and startup success stories",
+    "Build a platform for connecting remote freelance developers with clients",
+    "Develop a mobile app for learning and practicing meditation",
+    "Create an online marketplace for handmade home decor",
+    "Design a mobile game that teaches basic math skills",
+    "Start a blog about travel photography and adventure destinations",
+    "Build a platform for sharing and discovering healthy meal recipes",
+    "Develop a language learning app focused on business vocabulary",
+    "Create a social networking app for connecting fashion designers and enthusiasts",
+    "Design a website for sharing and reviewing books",
+    "Start a podcast about psychology and self-improvement",
+    "Build a platform for connecting remote freelance marketers with clients",
+    "Develop a mobile app for learning and practicing drawing",
+    "Create an online store for personalized gifts",
+    "Design a mobile game that promotes mental agility and problem-solving",
+    "Start a blog about sustainable gardening and urban farming",
+    "Build a platform for sharing and discovering motivational quotes",
+    "Develop a mobile app for finding and participating in local volunteer opportunities"
+    // Add more random ideas as needed
+);
+
+// Check if there are enough random ideas to fill the array
+if (count($randomIdeas) < 100) {
+    echo "Not enough random ideas available.";
+    exit();
+}
+
+// Fill the database with another 100 different random ideas
+$randomIndexes = array_rand($randomIdeas, 100);
+$i=0;
+foreach ($randomIndexes as $index) {
+  $content = $randomIdeas[$index];
+  $i++;
+  $room_id = rand (1,10);
+  $user_id = $i;
+  $status=1;
+
+    // uncomment to add more ideas to DB
+    //$idea->addIdea ($content, $user_id, $status, $order_importance=10, $updater_id=0, $votes_available_per_user=1, $info="", $room_id);
+    $ideas[] = $randomIdeas[$index];
+}
+// Get content of single idea
+$idea_id = 5;
+$result =  $idea->getIdeaContent ($idea_id);
+echo ("<br>Idea #".$idea_id." ".$crypt->decrypt ($result['displayname']).": ".$crypt->decrypt ($result['content'])." (".$result['created'].")");
+
+$offset = 0; // set start at dataset #10
+$limit = 5; // get 10 datasets
+out ("Reading multiple idea datasets (only active, status = 1) <br>using Idea class with limit ".$offset.",".$limit." ordered by id (4) Ascending (1)...",true);
+$ideadata = $idea->getIdeas($offset, $limit, 4, 1, 1);
+// idea list:
+foreach ($ideadata as $result) {
+    out ("ID: " . $result['id']);
+    out ("Name: " . $crypt->decrypt ($result['displayname']));
+    out ("Idea: " . $crypt->decrypt ($result['content']));
+    out ("Sum Votes: " . ($result['sum_votes']).", Sum Likes: " . ($result['sum_likes']));
+    out ("Last Update: " . $result['last_update']);
+    out ("created: " . $result['created']);
+}
+
+$offset = 0; // set start at dataset #0
+$limit = 5; // get 5 datasets
+$room_id = 2;
+// function getIdeasByRoom ($offset, $limit, $orderby=3, $asc=0, $status=1, $room_id)
+out ("Reading multiple idea datasets (of a certain room#".$room_id.") <br>using Idea class with limit ".$offset.",".$limit." ordered by id (4) Ascending (1)...",true);
+$ideadata = $idea->getIdeasByRoom($offset, $limit, 4, 1, 1, $room_id);
+// idea list:
+foreach ($ideadata as $result) {
+    out ("ID: " . $result['id']);
+    out ("Name: " . $crypt->decrypt ($result['displayname']));
+    out ("Idea: " . $crypt->decrypt ($result['content']));
+    out ("Sum Votes: " . ($result['sum_votes']).", Sum Likes: " . ($result['sum_likes']));
+    out ("Last Update: " . $result['last_update']);
+    out ("created: " . $result['created']);
+}
+
+
+$offset = 0; // set start at dataset #0
+$limit = 5; // get 5 datasets
+$user_id = 1;
+// function getIdeasByRoom ($offset, $limit, $orderby=3, $asc=0, $status=1, $room_id)
+out ("Reading multiple idea datasets (of a certain user#".$user_id.") <br>using Idea class with limit ".$offset.",".$limit." ordered by id (4) Ascending (1)...",true);
+$ideadata = $idea->getIdeasByUser($offset, $limit, 4, 1, 1, $user_id);
+// idea list:
+foreach ($ideadata as $result) {
+    out ("ID: " . $result['id']);
+    out ("Name: " . $crypt->decrypt ($result['displayname']));
+    out ("Idea: " . $crypt->decrypt ($result['content']));
+    out ("Sum Votes: " . ($result['sum_votes']).", Sum Likes: " . ($result['sum_likes']));
+    out ("Last Update: " . $result['last_update']);
+    out ("created: " . $result['created']);
+    out ("room #: " . $result['room_id']);
+}
+
+
+$offset = 0; // set start at dataset #0
+$limit = 5; // get 5 datasets
+$group_id = 9;
+// function getIdeasByRoom ($offset, $limit, $orderby=3, $asc=0, $status=1, $room_id)
+out ("Reading multiple idea datasets (of a certain group#".$group_id.") <br>using Idea class with limit ".$offset.",".$limit." ordered by id (4) Ascending (1)...",true);
+$ideadata = $idea->getIdeasByGroup($offset, $limit, 4, 1, 1, $group_id);
+// idea list:
+foreach ($ideadata as $result) {
+    out ("ID: " . $result['id']);
+    out ("Name: " . $crypt->decrypt ($result['displayname']));
+    out ("Idea: " . $crypt->decrypt ($result['content']));
+    out ("Sum Votes: " . ($result['sum_votes']).", Sum Likes: " . ($result['sum_likes']));
+    out ("Last Update: " . $result['last_update']);
+    out ("created: " . $result['created']);
+    out ("room #: " . $result['room_id']);
+}
+
+
+?>
