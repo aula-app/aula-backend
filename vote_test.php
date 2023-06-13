d12da87ccc6ee329f24ffcb0262f3d7e1d7b8e48
dkaula <133680228+dkaula@users.noreply.github.com>
2023-06-13 14:39:20 +0200

    Simple voting tester

---
+++ vote_test.php
@@ -0,0 +1,83 @@
+<?php
+require_once ('base_config.php'); // load base config with paths to classes etc.
+require_once ('error_msg.php');
+
+require ('functions.php'); // include Class autoloader (models)
+
+//load helper classes
+require_once ($baseHelperDir.'Crypt.php');
+
+// Create a new Database object with the MySQL credentials
+$db = new Database();
+$crypt = new Crypt($cryptFile); // path to $cryptFile is currently known from base_config.php -> will be changed later to be secure
+$syslog = new Systemlog ($db); // systemlog
+$idea = new Idea ($db, $crypt, $syslog); //, $syslog); // instanciate group model class
+$room = new Room ($db, $crypt, $syslog); // instanciate room model class
+$topic = new Topic ($db, $crypt, $syslog); // instanciate topic model class
+
+
+
+function out ($text, $form=false){ // lazy helper function
+  $formstart="";
+  $formend="";
+
+  if ($form) {
+    $formstart="<h2>";
+    $formend="</h2>";
+  }
+
+  echo ("<br>".$formstart.$text.$formend."<br>");
+}
+
+
+$user_id = 6;
+if (isset ($_REQUEST['user']))
+{
+  $user_id = intval ($_REQUEST['user']);
+}
+
+$vote_value = 1;
+if (isset ($_REQUEST['votevalue']))
+{
+  $vote_value = intval ($_REQUEST['votevalue']);
+}
+
+$idea_id = 0;
+
+if (isset ($_REQUEST['vote']))
+{
+  $idea_id = intval ($_REQUEST['vote']);
+  // voteForIdea($idea_id, $vote_value, $user_id)
+  echo ("<br>VOTING FOR idea: ".$idea_id.", user id: ".$user_id." value:".$vote_value);
+  $return_value = $idea->voteForIdea ($idea_id, $vote_value, $user_id);
+  echo ("Returning value: ".$return_value);
+} // end if
+
+?>
+<html>
+<body>
+
+<?php
+
+$offset = 0; // set start at dataset #10
+$limit = 5; // get 10 datasets
+out ("Reading idea datasets (only active, status = 1) <br>using Idea class with limit ".$offset.",".$limit." ordered by id (4) Ascending (1)...",true);
+out ("User 6 has a delegation from user 15", true);
+$ideadata = $idea->getIdeas($offset, $limit, 4, 1, 1);
+// idea list:
+foreach ($ideadata as $result) {
+    echo ('<form action="" method ="_POST">');
+    out ("ID: " . $result['id']);
+    out ("Name: " . $crypt->decrypt ($result['displayname']));
+    out ("Idea: " . $crypt->decrypt ($result['content']));
+    out ("Sum Votes: " . ($result['sum_votes']).", Sum Likes: " . ($result['sum_likes']));
+    out ("Last Update: " . $result['last_update']);
+    out ("created: " . $result['created']."<br><input type='hidden' name='vote' value='". intval (trim ($result['id']))."'><input type='hidden' name='votevalue' value='1'><br><button type=submit name='submitter'>VOTE +1</button></form><form method ='_POST'><input type='hidden' name='vote' value='". intval (trim ($result['id']))."'><input type='hidden' name='votevalue' value='-1'><button type=submit name='submitter'>VOTE -1</button></form>");
+    // User id <input type='text' name='user' value='". $user_id."'>
+    echo ("</form>");
+}
+
+?>
+
+</body>
+</html>
