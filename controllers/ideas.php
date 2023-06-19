60cd5f42d1b0c7f9456ff681f25131921bbe0597
Edgar Zanella Alvarenga <e@vaz.io>
2023-06-19 10:07:55 +0200

    Update ideas controller response

--- controllers/ideas.php
+++ controllers/ideas.php
@@ -12,12 +12,14 @@ $syslog = new Systemlog ($db);
 $idea = new Idea ($db, $crypt, $syslog); 
 $ideaData = $idea->getIdeas(0, 0, 4, 1, 1);
 
- $i = 0;
- foreach ($ideaData as $ideaRow) {
-   $ideaData[$i]['content'] = $crypt->decrypt ($ideaRow['content']);
-   $i = $i + 1;
- }
-
+$i = 0;
+$newData = array();
+foreach ($ideaData['data'] as $idea) {
+  $idea['content'] = $crypt->decrypt ($idea['content']);
+  array_push($newData, $idea);
+  $i = $i + 1;
+}
+$ideaData["data"] = $newData;
 header('Content-Type: application/json; charset=utf-8');
 echo json_encode($ideaData);
 
