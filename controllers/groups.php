a1c90ea98b0f32602f354d28fac58cb63a0a86e3
Edgar Zanella Alvarenga <e@vaz.io>
2023-06-20 13:10:04 +0200

    Add groups controllers

---
+++ controllers/groups.php
@@ -0,0 +1,18 @@
+<?php
+
+require_once ('../base_config.php'); // load base config with paths to classes etc.
+require_once ('../error_msg.php');
+require ('../functions.php'); // include Class autoloader (models)
+require ($baseHelperDir.'JWT.php');
+require_once ($baseHelperDir.'Crypt.php');
+
+$db = new Database();
+$crypt = new Crypt($cryptFile); // path to $cryptFile is currently known from base_config.php -> will be changed later to be secure
+$syslog = new Systemlog ($db); // systemlog
+$group = new Group ($db, $crypt, $syslog); // instanciate group model class
+$groupdata = $group->getGroups(0, 0, 4, 1, 1);
+
+header('Content-Type: application/json; charset=utf-8');
+echo json_encode($groupdata);
+
+?>
