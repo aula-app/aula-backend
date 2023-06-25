f35097a6cceef56ee2b1dac9645642a3eac21587
Edgar Zanella Alvarenga <e@vaz.io>
2023-06-25 18:14:45 +0200

    Add delete user controller

--- controllers/users.php
+++ controllers/users.php
@@ -10,21 +10,36 @@ $db = new Database();
 $crypt = new Crypt($cryptFile);
 $syslog = new Systemlog ($db);
 $user = new User ($db, $crypt, $syslog); 
-$userData = $user->getUsers($offset, $limit, 4, 1, 1);
+$jwt = new JWT($jwtKeyFile);
+$json = file_get_contents('php://input');
+$request_data = json_decode($json);
 
-$i = 0;
-$newData = array();
-foreach ($userData['data'] as $user) {
-  $user["realname"] = $crypt->decrypt ($user['realname']);
-  $user["username"] = $crypt->decrypt ($user['username']);
-  $user["displayname"] = $crypt->decrypt ($user['displayname']);
-  $user["email"] = $crypt->decrypt ($user['email']);
+$check_jwt = $jwt->check_jwt();
 
-  array_push($newData, $user);
-  $i = $i + 1;
+if ($check_jwt) {
+  $offset = $request_data->offset; 
+  $limit = $request_data->limit;
+  if (!$offset) {
+    $offset = 0;
+  }
+  if (!$limit) {
+    $limit = 0;
+  }
+  $userData = $user->getUsers($offset, $limit, 4, 1, 1);
+  $i = 0;
+  $newData = array();
+  foreach ($userData['data'] as $user) {
+    $user["realname"] = $crypt->decrypt ($user['realname']);
+    $user["username"] = $crypt->decrypt ($user['username']);
+    $user["displayname"] = $crypt->decrypt ($user['displayname']);
+    $user["email"] = $crypt->decrypt ($user['email']);
+  
+    array_push($newData, $user);
+    $i = $i + 1;
+  }
+  $userData["data"] = $newData;
+  header('Content-Type: application/json; charset=utf-8');
+  echo json_encode($userData);
 }
-$userData["data"] = $newData;
-header('Content-Type: application/json; charset=utf-8');
-echo json_encode($userData);
 
 ?>
