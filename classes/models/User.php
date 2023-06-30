3f2bc274c31ae23bb5ef34cc98e26cdb47746d3e
dkaula <133680228+dkaula@users.noreply.github.com>
2023-06-30 15:20:02 +0200

    Added order fields to user_basedata

--- classes/models/User.php
+++ classes/models/User.php
@@ -1422,7 +1422,6 @@ class User {
         $status = intval($status);
         $userlevel = intval ($userlevel);
 
-
         // check if user name is still available
         $temp_user = $this->checkUserExistsByUsername($username); // check username in db
         $temp_user_id = $temp_user ['data']; // get id from array
@@ -1441,7 +1440,7 @@ class User {
         // generate blind index
         $bi = md5 (strtolower (trim ($username)));
 
-        $stmt = $this->db->query('INSERT INTO '.$this->db->au_users_basedata.' (about_me, presence, auto_delegation, realname, displayname, username, email, pw, status, hash_id, created, last_update, updater_id, bi, userlevel) VALUES (:about_me, 1, 0, :realname, :displayname, :username, :email, :password, :status, :hash_id, NOW(), NOW(), :updater_id, :bi, :userlevel)');
+        $stmt = $this->db->query('INSERT INTO '.$this->db->au_users_basedata.' (o1, o2, o3, about_me, presence, auto_delegation, realname, displayname, username, email, pw, status, hash_id, created, last_update, updater_id, bi, userlevel) VALUES (:o1, :o2, :o3, :about_me, 1, 0, :realname, :displayname, :username, :email, :password, :status, :hash_id, NOW(), NOW(), :updater_id, :bi, :userlevel)');
         // bind all VALUES
         $this->db->bind(':username', $this->crypt->encrypt($username));
         $this->db->bind(':realname', $this->crypt->encrypt($realname));
@@ -1458,6 +1457,15 @@ class User {
         $hash_id = md5($username.$appendix); // create hash id for this user
         $this->db->bind(':hash_id', $hash_id);
         $this->db->bind(':updater_id', $updater_id); // id of the user doing the update (i.e. admin)
+        $o1 = mb_ord(strtolower(trim($username)));
+        $o2 = mb_ord(strtolower(trim($realname)));
+        $o3 = mb_ord(strtolower(trim($displayname)));
+        $this->db->bind(':o1', $o1);
+        $this->db->bind(':o2', $o2);
+        $this->db->bind(':o3', $o3);
+
+
+
 
         $err=false; // set error variable to false
 
