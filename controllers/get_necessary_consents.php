b4eb34bd13d072df6c8aae20731a351deb89c016
Edgar Zanella Alvarenga <e@vaz.io>
2023-07-04 19:30:21 +0200

    Add user consent controllers

---
+++ controllers/get_necessary_consents.php
@@ -0,0 +1,24 @@
+<?php
+
+require_once ('../base_config.php'); // load base config with paths to classes etc.
+require_once ('../error_msg.php');
+require ('../functions.php'); // include Class autoloader (models)
+require ($baseHelperDir.'JWT.php');
+require_once ($baseHelperDir.'Crypt.php');
+
+$db = new Database();
+$crypt = new Crypt($cryptFile);
+$syslog = new Systemlog ($db);
+$user = new User ($db, $crypt, $syslog); 
+$jwt = new JWT($jwtKeyFile);
+
+$check_jwt = $jwt->check_jwt();
+
+if ($check_jwt) {
+  $payload = $jwt->payload();
+  $user_id = $payload->user_id; 
+  $necessary_consents = $user->getMissingConsents($user_id);
+  echo json_encode($necessary_consents);
+}
+
+?>
