3578289adb16979bede2300549cba43403c2cb25
Edgar Zanella Alvarenga <e@vaz.io>
2023-07-05 12:08:16 +0200

    Fix login controller

--- controllers/login.php
+++ controllers/login.php
@@ -21,7 +21,7 @@ $json = file_get_contents('php://input');
 $data = json_decode($json);
 
 $loginResult = $user->checkLogin($data->username, $data->password);
-if ($loginResult["success"]) {
+if ($loginResult["success"] && $loginResult["error_code"] == 0) {
   $jwt_token = $jwt->gen_jwt($loginResult["data"]);
   echo json_encode(['JWT' => $jwt_token, "success" => true]);
 } else {
