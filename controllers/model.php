35f9c278d3319d30a021d25289d39c995bd32624
Edgar Zanella Alvarenga <e@vaz.io>
2024-03-05 14:06:22 +0100

    change arguments parsing from request

---
+++ controllers/model.php
@@ -0,0 +1,61 @@
+<?php
+
+require_once ('../base_config.php');
+require_once ('../error_msg.php');
+require ('../functions.php');
+require_once ($baseHelperDir.'Crypt.php');
+require_once ($baseHelperDir.'JWT.php');
+
+$db = new Database();
+$crypt = new Crypt($cryptFile);
+$syslog = new Systemlog ($db);
+$jwt = new JWT($jwtKeyFile);
+
+$json = file_get_contents('php://input');
+$input = json_decode($json, true);
+$check_jwt = $jwt->check_jwt();
+
+
+header('Content-Type: application/json; charset=utf-8');
+
+if ($check_jwt) {
+
+  $jwt_payload = $jwt->payload();
+  $user_id = $jwt_payload->user_id;
+
+  $model_name = $input["model"];
+  $model = new $model_name($db, $crypt, $syslog);
+  $method = $input["method"];
+  $arguments = $input["arguments"];
+
+  $decrypt_fields = $input["decrypt"];
+
+  $data = $model->$method(...$arguments);
+  
+  if ($data['error_code'] == 1) {
+    http_response_code(409);
+    echo json_encode(['success' => false, 'error' => 'Error fetching data for '.$model_name, 'detail' => $data]);
+  } else if ($data['success']){
+    http_response_code(200);
+
+  if ($data['data'] && count($data['data']) > 0) {
+    $newData = array();
+    foreach ($data['data'] as $item) {
+      foreach ($decrypt_fields as $field) {
+        $item[$field] = $crypt->decrypt($item[$field]);
+      }
+      array_push($newData, $item);
+    }
+    echo json_encode(['success' => true, 'data' => $newData]);
+    return;
+  }
+    echo json_encode(['success' => true, 'data' => $data]);
+  }
+
+} else {
+  http_response_code(401);
+  echo json_encode(['success' => false]);
+}
+
+
+?>
