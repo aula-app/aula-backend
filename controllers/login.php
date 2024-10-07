c51765313e199d30e465be44ad9bb662d4981460
Eduardo Liron <eduardoliron@gmail.com>
2024-10-07 16:00:18 -0300

    merge

--- controllers/login.php
+++ controllers/login.php
@@ -1,10 +1,10 @@
 <?php
 
-require_once ('../base_config.php'); // load base config with paths to classes etc.
-require_once ('../error_msg.php');
-require ('../functions.php'); // include Class autoloader (models)
-require_once ($baseHelperDir . 'Crypt.php');
-require_once ($baseHelperDir . 'JWT.php');
+require_once('../base_config.php'); // load base config with paths to classes etc.
+require_once('../error_msg.php');
+require('../functions.php'); // include Class autoloader (models)
+require_once($baseHelperDir . 'Crypt.php');
+require_once($baseHelperDir . 'JWT.php');
 
 $db = new Database();
 $crypt = new Crypt($cryptFile); // path to $cryptFile is currently known from base_config.php -> will be changed later to be secure
@@ -27,8 +27,10 @@ $loginResult = $user->checkLogin($data->username, $data->password);
 if ($loginResult["success"] && $loginResult["error_code"] == 0) {
   $current_settings = $settings->getInstanceSettings();
   if ($current_settings["data"]["online_mode"] != 1 && $loginResult["data"]["userlevel"] < 50) {
-    echo json_encode(["success" => "false",
-    "online_mode" => $current_settings["data"]["online_mode"]]);
+    echo json_encode([
+      "success" => "false",
+      "online_mode" => $current_settings["data"]["online_mode"]
+    ]);
     return;
   }
 
