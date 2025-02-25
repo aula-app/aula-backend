e4713e3110805ac9c3aab65522f654e563b976cc
Edgar Zanella Alvarenga <e@vaz.io>
2025-02-25 11:41:14 +0100

    Add permissions for Users.getUsersByRoom

--- classes/helpers/Permissions.php
+++ classes/helpers/Permissions.php
@@ -394,6 +394,16 @@ function checkPermissions($model_name, $model, $method, $arguments, $user_id, $u
     ],
 
     "User" => [
+      "getUsersByRoom" => [
+        "open_roles" => [
+           "super_moderator",
+           "super_moderator_v",
+           "principal",
+           "principal_v",
+           "admin"
+        ],
+      ],
+
       "refresh_token" => [
         "roles" => [ "all" ]
       ],
