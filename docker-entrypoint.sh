2928522d3b34d1fe04a56477a80bcbde32bf5631
Nikola Marić <206777381+nikola-maric-aula@users.noreply.github.com>
2025-06-05 22:53:43 +0200

    chore: cleanup comments

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -8,18 +8,13 @@ if [[ "$JWT_KEY" == "CHANGE_ME" || "$SUPERKEY" == "CHANGE_ME" ]]; then
   exit 1;
 fi
 
-# write the super keys from environment variables, where they
-#  should be kept.
+# write the super keys from environment variables, where they should be kept.
 echo $JWT_KEY > config/jwt_key.ini
 echo $SUPERKEY > config/superkey.ini
-
-# maye you will need this information at startup
-# php -i
 
 # run apache as expected
 /usr/sbin/apache2ctl -D FOREGROUND & 
 
-# make sure to show php errors in the docker log
-#  while it is running
+# make sure to show php errors in the docker log while it is running
 tail -f /var/log/apache2/error.log
 
