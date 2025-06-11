e90590864af0c43ceaf6ef2329ceedc3c4d640b3
Nikola Marić <206777381+nikola-maric-aula@users.noreply.github.com>
2025-06-11 16:39:41 +0200

    feat: run-local - offer to regenerate d-c.override.yml
    * fix: random keys shouldn't contain `&` sign so the sed substition
      would work properly

--- docker-prepare-local.sh
+++ docker-prepare-local.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
 
+set -e
+
 echo "[*] Preparing environment..."
 
 echo "[*] Copying example config files... (generated files are NOT PRODUCTION READY)"
 cp ./config/base_config.php-example ./config/base_config.php
 cp ./config/db_config.ini-example ./config/db_config.ini
+
+if [ -f ./docker-compose.override.yml ]; then
+  echo "[*] You already have docker-compose.override.yml."
+  REPLY=$(read -p "-> Do you want to regenerate it? [y/N] " -r)
+  if [[ ! $REPLY =~ ^[Yy]$ ]]; then
+    rm -fv ./docker-compose.override.yml
+  fi
+fi
 
 if [ ! -f ./docker-compose.override.yml ]; then
   echo "[*] Adapting docker-compose.override.yml to your local environment..."
@@ -14,10 +24,11 @@ if [ ! -f ./docker-compose.override.yml ]; then
   sed -i.bak "s/UID/$(id -u)/g" docker-compose.override.yml
   sed -i.bak "s/GID/$(id -g)/g" docker-compose.override.yml
 
-  ENCRYPTION_KEY_1=$(LC_ALL=C tr -dc 'A-Za-z0-9#%&()+,-.:;<=>@^_~' </dev/urandom | head -c 64; echo)
-  ENCRYPTION_KEY_2=$(LC_ALL=C tr -dc 'A-Za-z0-9#%&()+,-.:;<=>@^_~' </dev/urandom | head -c 64; echo)
+  ENCRYPTION_KEY_1=$(LC_ALL=C tr -dc 'A-Za-z0-9#%()+,-.:;<=>@^_~' </dev/urandom | head -c 64; echo)
+  ENCRYPTION_KEY_2=$(LC_ALL=C tr -dc 'A-Za-z0-9#%()+,-.:;<=>@^_~' </dev/urandom | head -c 64; echo)
   sed -i.bak "s/JWT_KEY: CHANGE_ME/JWT_KEY: \"$ENCRYPTION_KEY_1\"/g" docker-compose.override.yml
   sed -i.bak "s/SUPERKEY: CHANGE_ME/SUPERKEY: \"$ENCRYPTION_KEY_2\"/g" docker-compose.override.yml
+  rm -vf ./docker-compose.override.yml.bak
 else
   echo "[*] Reusing existing docker-compose.override.yml... Delete it to regenerate it (you will lose the random keys)."
 fi
