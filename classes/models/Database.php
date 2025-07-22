4acb8e263e526a7a84640938f63c064337fccabd
Nikola Marić <206777381+nikola-maric-aula@users.noreply.github.com>
2025-07-22 17:35:07 +0200

    feat: editUser - check for existing new email/username
    * fix: addUser or addUserCSV - fix checking for existing email/username

--- classes/models/Database.php
+++ classes/models/Database.php
@@ -87,23 +87,31 @@
     }
 
     public function bind($param, $value, $type = null) {
-        # provides binding functionality
-        if (is_null($type)) {
-            switch (true) {
-                case is_int($value):
-                    $type = PDO::PARAM_INT;
-                    break;
-                case is_bool($value):
-                    $type = PDO::PARAM_BOOL;
-                    break;
-                case is_null($value):
-                    $type = PDO::PARAM_NULL;
-                    break;
-                default:
-                    $type = PDO::PARAM_STR;
+        if (str_starts_with($param, ':')) {
+            # provides binding functionality
+            if (is_null($type)) {
+                switch (true) {
+                    case is_int($value):
+                        $type = PDO::PARAM_INT;
+                        break;
+                    case is_bool($value):
+                        $type = PDO::PARAM_BOOL;
+                        break;
+                    case is_null($value):
+                        $type = PDO::PARAM_NULL;
+                        break;
+                    default:
+                        $type = PDO::PARAM_STR;
+                }
             }
+            $this->stmt->bindValue($param, $value, $type);
         }
-        $this->stmt->bindValue($param, $value, $type);
+    }
+
+    public function bindAll($keyvalues) {
+        foreach ($keyvalues as $key => $value) {
+            $this->bind($key, $value);
+        }
     }
 
     public function execute() {
@@ -161,6 +169,4 @@
     public function getDbname() {
         return $this->dbname;
     }
-    
 }
-?>
