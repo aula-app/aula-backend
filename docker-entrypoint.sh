1b08807e0010c7a283428c52b2428283f506a3ae
Nikola Marić <206777381+nikola-maric-aula@users.noreply.github.com>
2025-08-07 11:46:28 +0200

    fix: apache config
    * hide folders listings, forbidden folders, and trailing slash redirects by returning 404

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,18 +2,18 @@
 
 echo "[*] Preparing the runtime environment for the docker application..."
 
-# add our local user to apache run group so we can live edit files
+mkdir -p ./config && mkdir -p ./files && \
+  mkdir -p ./errors && chmod 755 ./errors && \
+  echo -n '{"success":false,"error":"API_NOT_FOUND"}' > ./errors/404.json
+
 if [[ "$APP_ENV" == "local" ]]; then
+  # add our local user to apache run group so we can live edit files
   TARGET_USER_NAME=$(ls -l ./config/base_config.php | awk '{ print $3 }')
   usermod -aG $APACHE_RUN_GROUP $TARGET_USER_NAME
-  mkdir -p ./config && mkdir -p ./files && \
-    mkdir -p ./empty && chmod 600 ./empty
+  chown -R $APACHE_RUN_GROUP:$APACHE_RUN_USER ./errors
 else
-  echo -n '{"success":false,"error":"API_NOT_FOUND"}' > "./error.json"
   # set up directory structure in the container
-  mkdir -p ./config && mkdir -p ./files && \
-    chown -R $APACHE_RUN_GROUP:$APACHE_RUN_USER ./ && \
-    mkdir -p ./empty && chmod 600 ./empty
+  chown -R $APACHE_RUN_GROUP:$APACHE_RUN_USER ./
 fi
 
 if [[ "$SUPERKEY" == "CHANGE_ME" ]]; then
@@ -21,7 +21,6 @@ if [[ "$SUPERKEY" == "CHANGE_ME" ]]; then
   echo "[ERROR] Please update the environment variables (probably in the docker-compose.yml file)."
   exit 1;
 fi
-
 
 echo "[✓] Runtime environment prepared. Starting apache2 server."
 # Start the apache server in foreground (so Docker doesn't exit)
