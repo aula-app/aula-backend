d80ab4a3ac809536926205053101c77dc860a91f
Nikola Marić <206777381+nikola-maric-aula@users.noreply.github.com>
2025-09-08 14:30:09 +0200

    ref(enum): const to enum; refactoring in Command model TBD

--- src/classes/usecases/commands/CommandHandler.php
+++ src/classes/usecases/commands/CommandHandler.php
@@ -1,14 +1,17 @@
 <?php
 
+enum CommandStatus: int
+{
+  case NOT_RUN = 0;
+  case SUCCESS = 1;
+  case EXECUTION_FAILURE = 2;
+  case VALIDATION_FAILED = 3;
+  case EXECUTION_EXCEPTION = 4;
+}
+
 abstract class CommandHandler
 {
   private $commandModel;
-
-  private const COMMAND_STATUS_NOT_RUN = 0;
-  private const COMMAND_STATUS_SUCCESS = 1;
-  private const COMMAND_STATUS_EXECUTION_FAILURE = 2;
-  private const COMMAND_STATUS_VALIDATION_FAILED = 3;
-  private const COMMAND_STATUS_EXECUTION_EXCEPTION = 4;
 
   public function __construct(protected $db, protected $crypt, protected $syslog)
   {
@@ -25,25 +28,26 @@ abstract class CommandHandler
     try {
       $validationResult = $this->isValid($command);
       if ($validationResult != true) {
-        $this->commandModel->setCommandStatus($id, self::COMMAND_STATUS_VALIDATION_FAILED);
+        $this->commandModel->setCommandStatus($id, CommandStatus::VALIDATION_FAILED->value);
       }
 
       // @TODO: nikola - use db transaction with SELECT .. FOR UPDATE for isolation
 
       // @TODO: nikola - ensure no other handler is running the same command at the same time
       //   (due to retries and long-running commands or multiple concurrent BE services)
+      //   try setting active = 2 (RUNNING) and when done (or on error), set active = 0 (or 1 if retry makes sense)
       $returnValue = $this->execute($command);
 
       if ($returnValue['error_code'] == 0) {
-        $this->commandModel->setCommandStatus($id, self::COMMAND_STATUS_SUCCESS);
+        $this->commandModel->setCommandStatus($id, CommandStatus::SUCCESS->value);
         $this->commandModel->setActiveStatus($id, 0);
       } else {
-        $this->commandModel->setCommandStatus($id, self::COMMAND_STATUS_EXECUTION_FAILURE);
+        $this->commandModel->setCommandStatus($id, CommandStatus::EXECUTION_FAILURE->value);
         $this->commandModel->setActiveStatus($id, 0);
       }
     } catch (Exception $err) {
       error_log("Command execution ERROR " . $err->getMessage());
-      $this->commandModel->setCommandStatus($id, self::COMMAND_STATUS_EXECUTION_EXCEPTION);
+      $this->commandModel->setCommandStatus($id, CommandStatus::EXECUTION_EXCEPTION->value);
     }
   }
 
