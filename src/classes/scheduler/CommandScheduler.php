972397ed0a69cfcf968ad42492d031c4e697ecbb
nikola-maric-aula <nikola.maric@aula.de>
2025-09-30 16:13:44 +0200

    fix(cron): scheduler fails to run due to wrong import (#298)
    ## Context <!-- ie. explanations, background, documentation -->
    
    <!-- Example:
    After the last update it became apparent that we did fetch the new
    results, but didn't actually store them properly. This caused a glitch
    in the UI whenever the user would refresh the page.
    -->
    
    ## Checklist
    
    - [ ] Tested manually <!-- you can strikethrough this option in case you
    haven't tested manually -->
    - [ ] GitHub issue linked <!-- Use the "Development" field of the Issue,
    or add a link if it's outside this Repo -->
    - [ ] Changelist updated
    - [ ] Backward and forward compatible with FE <!-- If not, please
    describe in detail and include other PR links -->
    - [ ] Doesn't need update in the database <!-- If it does, please
    describe how to deploy it without downtime -->
    - [ ] Must be deployed ASAP (HOTFIX)

--- src/classes/scheduler/CommandScheduler.php
+++ src/classes/scheduler/CommandScheduler.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once(__DIR__ . '/../../config/base_config.php');
+require_once(__DIR__ . '/../../../config/base_config.php');
 require_once(__DIR__ . '/../../functions.php');
 
 global $baseHelperDir;
