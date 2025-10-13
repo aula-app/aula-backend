585e40f03c6522f3b301bbaa2fb7da1bbedbbb22
Nikola Marić <206777381+nikola-maric-aula@users.noreply.github.com>
2025-10-13 16:25:14 +0200

    ref: move docker setup into legacy/

--- legacy/docker-prepare-local.sh
+++ legacy/docker-prepare-local.sh
@@ -5,12 +5,12 @@ set -e
 echo "[*] Preparing local environment configuration..."
 
 echo "[*] Copying example config files... (generated files are NOT PRODUCTION READY)"
-[ -f ./legacy/config/base_config.php ] && \
+[ -f ./config/base_config.php ] && \
   echo "  [*] You already have ./config/base_config.php. Delete it to regenerate it." || \
-  cp -n ./legacy/config/base_config.php-example ./legacy/config/base_config.php
-[ -f ./legacy/config/instances_config.php ] && \
+  cp -n ./config/base_config.php-example ./config/base_config.php
+[ -f ./config/instances_config.php ] && \
   echo "  [*] You already have ./config/instances_config.php. Delete it to regenerate it." || \
-  cp -n ./legacy/config/instances_config.php-example ./legacy/config/instances_config.php
+  cp -n ./config/instances_config.php-example ./config/instances_config.php
 
 if [ -f ./docker-compose.override.yml ]; then
   echo "[*] You already have docker-compose.override.yml."
