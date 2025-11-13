21e0a5e7ac7d6a0c0026018091b23e8aba2368a3
Nikola Marić <206777381+nikola-maric-aula@users.noreply.github.com>
2025-11-13 17:43:47 +0100

    feat(auth): User models added

---
+++ app/Models/Tenant.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Stancl\Tenancy\Database\Concerns\HasDatabase;
+use Stancl\Tenancy\Database\Contracts\TenantWithDatabase;
+use Stancl\Tenancy\Database\Models\Tenant as BaseTenant;
+
+class Tenant extends BaseTenant implements TenantWithDatabase
+{
+    use HasDatabase;
+
+    public static function getCustomColumns(): array
+    {
+        return array_merge(parent::getCustomColumns(), [
+            'name',
+            'api_base_url',
+            'contact_info',
+            'admin_name',
+            'admin_username',
+            'admin_email',
+            'admin_init_pass_url',
+            'tech_admin_name',
+            'tech_admin_username',
+            'tech_admin_email',
+            'tech_admin_init_pass_url',
+            'instance_code',
+        ]);
+    }
+}
