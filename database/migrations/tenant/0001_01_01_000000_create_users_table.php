a3e4a57a17525bd2ef68e9cb35c8996249a0f44f
Nikola Marić <206777381+nikola-maric-aula@users.noreply.github.com>
2025-12-11 17:00:08 +0100

    feat: use UUIDv4 for ids

--- database/migrations/tenant/0001_01_01_000000_create_users_table.php
+++ database/migrations/tenant/0001_01_01_000000_create_users_table.php
@@ -12,7 +12,8 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->uuid('id')->primary();
+            // use mariadb-specific UUID_v4() function to initialize id field
+            $table->uuid('id')->primary()->default(DB::raw('UUID_v4()'));
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
