1478fc52b67bacee079201b21c21ba092aae370c
Jakob Wierzba <git@jakobwierzba.de>
2026-06-09 15:14:43 +0200

    use constructors in UseCases

--- app/UseCases/User/ShowUserUseCase.php
+++ app/UseCases/User/ShowUserUseCase.php
@@ -7,7 +7,7 @@ use App\Models\LegacyUser;
 
 class ShowUserUseCase
 {
-    public static function execute(string $id): UserData
+    public function execute(string $id): UserData
     {
         $legacyUser = LegacyUser::findOrFail($id);
         return UserData::from($legacyUser);
