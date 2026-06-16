f9205eb888f48b6e2d89e858fd00ab4bd38cf019
Jakob Wierzba <git@jakobwierzba.de>
2026-06-16 10:08:14 +0200

    fix controller dependency injection

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -17,34 +17,41 @@ use App\Data\UserUpdateData;
 
 class UserController extends Controller
 {
+    public function __construct(
+        protected CreateUserUseCase $createUserUseCase,
+        protected ShowUserUseCase $showUserUseCase,
+        protected ListUsersUseCase $listUsersUseCase,
+        protected UpdateUserUseCase $updateUserUseCase,
+        protected DeleteUserUseCase $deleteUserUseCase,
+    ) {}
+
     public function index(): DataCollection
     {
         // TODO: implement
         // - pagination
         // - sorting
         // - filter by status, userlevel, room_id?
-        return ListUsersUseCase::execute();
+        return $this->listUsersUseCase->execute();
     }
 
     // TODO? hash_id is nullable in DB
     public function show(string $hashId): UserModelData
     {
-        return (new ShowUserUseCase())->execute($hashId);
+        return $this->showUserUseCase->execute($hashId);
     }
 
     public function store(UserStoreData $userStoreData): UserModelData
     {
-        // $user = User::fromRequest($storeUserRequest);
-        return (new CreateUserUseCase())->execute($userStoreData);
+        return $this->createUserUseCase->execute($userStoreData);
     }
 
     public function update(string $hashId, UserUpdateData $userUpdateData): UserModelData
     {
-        return (new UpdateUserUseCase())->execute($hashId, $userUpdateData);
+        return $this->updateUserUseCase->execute($hashId, $userUpdateData);
     }
 
     public function destroy(string $hashId): void
     {
-        (new DeleteUserUseCase())->execute($hashId);
+        $this->deleteUserUseCase->execute($hashId);
     }
 }
