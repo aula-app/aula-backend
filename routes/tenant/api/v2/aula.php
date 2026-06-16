6c91324eec1f6365a20d2f0e435c5e23dac2a002
Jakob Wierzba <git@jakobwierzba.de>
2026-06-16 11:18:31 +0200

    validate route param user/uuid

--- routes/tenant/api/v2/aula.php
+++ routes/tenant/api/v2/aula.php
@@ -4,5 +4,9 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 
 // TODO: GET /api/v2/users/ without header is 500ing with TenantCouldNotBeIdentifiedByRequestDataException, should probably 400/404?
-Route::apiResource('users', UserController::class)->except(['update']);
-Route::put('users/{user}', [UserController::class, 'update'])->name('users.update');
+Route::apiResource('users', UserController::class)
+    ->except(['update'])
+    ->whereUuid('user');
+Route::put('users/{user}', [UserController::class, 'update'])
+    ->name('users.update')
+    ->whereUuid('user');
