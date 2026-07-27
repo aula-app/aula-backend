955f7082b774c7abbdd820469b0f596170ac78bc
Edgar Zanella Alvarenga <e@vaz.io>
2026-07-27 13:43:41 +0200

    Feat/tenant export import clean (#482)
    ## Context <!-- ie. explanations, background, documentation -->
    
    Fix https://github.com/aula-app/aula-manager/issues/42
    
    A tenant can be now exported with: php artisan tenant:export --code 
    This generates a .tar.gz file containing a json with the instance
    details and a sql for the instance database.
    
    The tenant can be import with: php artisan tenant:import --code=NEW-CODE
    --name=NEW-NAME ./tenant_INSTANCE_CODE_20260430_141745.tar.gz
    
    - [x] Tested manually <!-- you can strikethrough this option in case you
    haven't tested manually -->
    - [x] GitHub issue linked <!-- Use the "Development" field of the Issue,
    or add a link if it's outside this Repo -->
    - [x] Changelist updated
    - [x] Backward and forward compatible with
    [aula-frontend/releases](https://github.com/aula-app/aula-frontend/releases)
    <!-- If not, please describe in detail and include other PR links -->
    - [x] Doesn't need update in the database <!-- If it does, please
    describe how to deploy it without downtime -->
    - [ ] Must be deployed ASAP (HOTFIX)
    - [ ] Needs update of [docs.aula.de](https://docs.aula.de/)
    ([repo](https://github.com/leonard-haas/docs_aula)) <!-- If it does,
    please ping Leonard OR include link to the change in the docs repo -->
    
    ---------
    
    Co-authored-by: Jakob Wierzba <git@jakobwierzba.de>

---
+++ tests/Feature/ExportTenantCommandTest.php
@@ -0,0 +1,99 @@
+<?php
+
+declare(strict_types=1);
+
+use App\Models\Tenant;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Illuminate\Support\Facades\Process;
+use Illuminate\Support\Str;
+
+uses(DatabaseTransactions::class);
+
+function createTenantForExport(array $attrs = []): Tenant
+{
+    // withoutEvents prevents stancl from running CreateDatabase (DDL that commits the transaction)
+    return Tenant::withoutEvents(function () use ($attrs) {
+        $tenant = new Tenant(array_merge([
+            'name'            => 'Export School',
+            'instance_code'   => 'EXP01',
+            'jwt_key'         => 'testkey',
+            'api_base_url'    => 'https://example.com',
+            'admin1_name'     => 'Admin One',
+            'admin1_username' => 'admin1',
+            'admin1_email'    => 'admin1@test.com',
+            'admin2_name'     => 'Admin Two',
+            'admin2_username' => 'admin2',
+            'admin2_email'    => 'admin2@test.com',
+            'tenancy_db_name' => 'tenant_test_export',
+        ], $attrs));
+        $tenant->id = (string) Str::uuid();
+        $tenant->save();
+
+        return $tenant;
+    });
+}
+
+it('fails when neither --code nor --id is provided', function () {
+    $this->artisan('tenant:export')->assertFailed();
+});
+
+it('fails when tenant is not found by code', function () {
+    $this->artisan('tenant:export', ['--code' => 'XXXXX'])->assertFailed();
+});
+
+it('fails when tenant is not found by id', function () {
+    $this->artisan('tenant:export', ['--id' => '00000000-0000-0000-0000-000000000000'])->assertFailed();
+});
+
+it('exports successfully by code', function () {
+    createTenantForExport();
+
+    Process::fake([
+        "'mysqldump'*" => Process::result(),
+        "'tar'*"       => Process::result(),
+        "'rm'*"        => Process::result(),
+    ]);
+
+    $this->artisan('tenant:export', [
+        '--code'   => 'EXP01',
+        '--output' => '/tmp/test_export_code.tar.gz',
+    ])->assertSuccessful();
+});
+
+it('exports successfully by id', function () {
+    $tenant = createTenantForExport();
+
+    Process::fake([
+        "'mysqldump'*" => Process::result(),
+        "'tar'*"       => Process::result(),
+        "'rm'*"        => Process::result(),
+    ]);
+
+    $this->artisan('tenant:export', [
+        '--id'     => $tenant->id,
+        '--output' => '/tmp/test_export_id.tar.gz',
+    ])->assertSuccessful();
+});
+
+it('fails when mysqldump fails', function () {
+    createTenantForExport();
+
+    Process::fake([
+        "'mysqldump'*" => Process::result('', 'Connection refused', 1),
+        "'rm'*"        => Process::result(),
+    ]);
+
+    $this->artisan('tenant:export', ['--code' => 'EXP01'])->assertFailed();
+});
+
+it('fails when tar fails', function () {
+    createTenantForExport();
+
+    Process::fake([
+        "'mysqldump'*" => Process::result(),
+        "'tar'*"       => Process::result('', 'No space left on device', 1),
+        "'rm'*"        => Process::result(),
+    ]);
+
+    $this->artisan('tenant:export', ['--code' => 'EXP01'])->assertFailed();
+});
