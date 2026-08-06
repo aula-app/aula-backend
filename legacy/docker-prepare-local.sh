8422cb786087cd90d1c85027e88b685cd0db64ae
Nikola Marić <206777381+nikola-maric-aula@users.noreply.github.com>
2026-08-06 17:58:17 +0200

    feat: legacy can verify assymetric key used inv2 JWT

--- legacy/docker-prepare-local.sh
+++ legacy/docker-prepare-local.sh
@@ -5,8 +5,13 @@ set -e
 echo "[*] Preparing local environment configuration..."
 
 echo "[*] Copying example config files... (generated files are NOT PRODUCTION READY)"
+
 [ -f ./config/base_config.php ] && \
   echo "  [*] You already have ./config/base_config.php. Delete it to regenerate it." || \
   cp -n ./config/base_config.php-example ./config/base_config.php
+
+[ -f ./config/oauth-public.key ] && \
+  echo "  [*] You already have ./config/oauth-public.key. Delete it to regenerate it." || \
+  sudo cp -n ../storage/oauth-public.key ./config/oauth-public.key
 
 echo "[✓] Local environment prepared."
