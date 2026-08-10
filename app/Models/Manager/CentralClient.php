d75d24d0d08780164b4fee8e92ff71b4fcc24249
Nikola Marić <206777381+nikola-maric-aula@users.noreply.github.com>
2026-08-10 10:57:12 +0200

    chore: docs and cleanup

--- app/Models/Manager/CentralClient.php
+++ app/Models/Manager/CentralClient.php
@@ -4,7 +4,21 @@ namespace App\Models\Manager;
 
 use Laravel\Passport\Client as PassportClient;
 
+/**
+ * Defining our own PassportClient class in order to
+ * ensure the Mobile App (and other API Clients) are not
+ * Tenant-specific. Until further change of requirements.
+ */
 class CentralClient extends PassportClient
 {
-    protected $connection = 'mariadb_central'; // config('tenancy.database.central_connection');
+    /**
+    * The value must be hardcoded. It must match the value of
+    *   config('tenancy.database.central_connection'), which in
+    *   turn needs to be found in config('database.connections.*').
+    *
+    * See ./config/database.php and ./config/tenancy.php.
+    *
+    * @var string
+    */
+    protected $connection = 'mariadb_central';
 }
