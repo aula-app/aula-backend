ab4d27d6129c586205691793627de7288e39d923
Nikola Marić <206777381+nikola-maric-aula@users.noreply.github.com>
2026-09-15 11:22:23 +0200

    feat: track migrated users in au_systemlog

--- database/migrations/tenant/2026_09_08_145318_convert_techadmin_users_to_admins.php
+++ database/migrations/tenant/2026_09_08_145318_convert_techadmin_users_to_admins.php
@@ -2,11 +2,19 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
 
-return new class extends Migration
-{
+return new class () extends Migration {
     public function up(): void
     {
+        $users = DB::table('au_users_basedata')
+            ->where('userlevel', 60)
+            ->get(['id', 'username']);
+        $numOfUsers = $users->count();
+        $message = "Migrating [{$numOfUsers}] from techadmin to admin userlevel: " . $users->toJson();
+        Log::info($message, [ 'tenant' => tenant('instance_code'), 'count' => $numOfUsers ]);
+
+        DB::table('au_systemlog')->insert([ 'message' => $message ]);
         DB::statement('UPDATE au_users_basedata SET userlevel=50, refresh_token=1 WHERE userlevel=60');
     }
 
